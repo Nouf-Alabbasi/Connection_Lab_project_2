4c4b85ce336a5772bc3b856405b93fba6a9248ff
hashimzia <hz2776@nyu.edu>
2022-11-06 16:54:59 +0400

    added sockets

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+let http = require('http');
+let server = http.createServer(app);
+
+app.use('/', express.static('public'));
+server.listen(3000, () => {
+    console.log('listening on 3000...');
+})
+
+let io = require('socket.io')
+io = new io.Server(server);
+let no_of_connections = 0;
+io.on('connect', (socket) => {
+    console.log('New Connection:', socket.id);
+    no_of_connections++;
+    if (no_of_connections == 1) {
+        socket.role = "seeker";
+    }
+    else {
+        socket.role = "hider";
+    }
+
+    socket.on('hide', (data) => {
+        console.log(data);
+    })
+
+
+
+
+    socket.on('disconnect', (socket) => {
+        console.log('Disconnected');
+        no_of_connections--;
+    })
+})
